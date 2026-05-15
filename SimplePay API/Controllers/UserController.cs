35abbdeadb9084f1a35fd2070ec3d98a8a479b89
nmedina <nicolascostam23@gmail.com>
2026-05-15 19:55:28 -0300

    :sparkles: feat: Add a UserController method GetAllUsers

---
+++ SimplePay API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePay_API.Context;
+using SimplePay_API.Models;
+using System.Collections;
+
+namespace SimplePay_API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly AppDbContext? _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<User>> GetAllUsers()
+    {
+        var listUsers = _context.Users.ToList();
+        
+        if(listUsers == null) return NotFound("Usuarios nao encotrado...");
+
+        return Ok(listUsers);
+
+    }
+}
