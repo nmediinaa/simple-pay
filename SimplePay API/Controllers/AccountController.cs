e2c57927a6e2853b42d8be3f6d52372e1d8529dd
nmedina <nicolascostam23@gmail.com>
2026-05-21 18:28:53 -0300

    :sparkles: feat: implement error handling with try-catch blockon AccountController

--- SimplePay API/Controllers/AccountController.cs
+++ SimplePay API/Controllers/AccountController.cs
@@ -19,45 +19,74 @@ public class AccountController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Account>> GetAllAccounts()
     {
-        var listAccounts = _context.Accounts
+        try
+        {
+            var listAccounts = _context.Accounts
             .Include(a => a.User)
             .AsNoTracking()
             .ToList();
 
-        if (listAccounts == null) return NotFound();
+            if (listAccounts == null) return NotFound();
 
-        return Ok(listAccounts);
+            return Ok(listAccounts);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao processar sua requisicao...");
+        }
+        
+        
     }
 
     [HttpGet("{id:int}")]
     public ActionResult GetAccountById(int id)
     {
-        var account = _context.Accounts
+
+        try
+        {
+            var account = _context.Accounts
             .AsNoTracking()
             .FirstOrDefault(a => a.AccountId == id);
+
+            if (account == null) return NotFound($"Conta de id = {id} nao encontrada...");
+            return Ok(account);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao processar sua requisicao...");
+        }
         
-        if(account == null) return NotFound("Conta nao encontrada...");
-        return Ok(account);
     }
 
 
     [HttpPost]
     public ActionResult CreateAccount(Account account)
     {
-        bool isAccountExist = _context.Accounts.Any(a => a.AccountId == account.AccountId);
 
-        if (isAccountExist) return BadRequest("A conta ja exite...");
-        
-        int userId = account.UserId;
-        var isIdValid = _context.Users.Any(u => u.Id == userId);
+        try
+        {
+            bool isAccountExist = _context.Accounts.Any(a => a.AccountId == account.AccountId);
 
-        if (!isIdValid) return BadRequest("User Id nao e valido...");
+            if (isAccountExist) return BadRequest($"A conta com o id = {account.AccountId} ja existe...");
+
+            int userId = account.UserId;
+            var isIdValid = _context.Users.Any(u => u.Id == userId);
+
+            if (!isIdValid) return BadRequest($"O User Id = {userId} nao e valido...");
 
 
-        _context.Accounts.Add(account);
-        _context.SaveChanges();
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
 
-        return CreatedAtAction(nameof(GetAccountById), new { id = account.AccountId }, account);
+            return CreatedAtAction(nameof(GetAccountById), new { id = account.AccountId }, account);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao processar sua requisicao...");
+        }
+       
     }
 
 }
