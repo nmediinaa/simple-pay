5e04e378fa5584e9ac15e22594f2d7a80398e250
nmediinaa <nicolascostam23@gmail.com>
2026-06-29 22:33:47 -0300

    :recycle: refactor: abstract data access logic into a generic repository

--- SimplePay API/Repositories/AccountRepository.cs
+++ SimplePay API/Repositories/AccountRepository.cs
@@ -4,47 +4,9 @@ using SimplePay_API.Repositories.Interfaces;
 
 namespace SimplePay_API.Repositories;
 
-public class AccountRepository : IAccountRepository
+public class AccountRepository : Repository<Account>, IAccountRepository
 {
-    private readonly AppDbContext _context;
-
-    public AccountRepository(AppDbContext context)
+    public AccountRepository(AppDbContext context) : base(context)
     {
-        _context = context;
     }
-
-    public Account CreateAccount(Account account)
-    {
-        if(account == null) throw new ArgumentNullException(nameof(account));
-
-        _context.Accounts.Add(account);
-        _context.SaveChanges();
-
-        return account;
-        
-    }
-
-    public Account DeleteAccount(int id)
-    {
-        var accountToRemove = _context.Accounts.Find(id);
-
-        if(id == null) throw new ArgumentNullException(nameof(accountToRemove));
-
-        _context.Accounts.Remove(accountToRemove);
-        _context.SaveChanges();
-
-        return accountToRemove;
-
-    }
-
-    public Account GetAccountById(int id)
-    {
-        return _context.Accounts.FirstOrDefault(a => a.AccountId == id);
-    }
-
-    public IEnumerable<Account> GetAllAccounts()
-    {
-        return _context.Accounts.ToList();
-    }
-
 }
---
+++ SimplePay API/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using SimplePay_API.Context;
+using SimplePay_API.Repositories.Interfaces;
+
+namespace SimplePay_API.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class //Proteção contra a herança de classes que não sejam do tipo class
+{
+    protected readonly AppDbContext _context; //Aqui todas classes derivadas terão acesso ao contexto do banco de dados
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList(); //Set é um método do DbContext que retorna um DbSet especifico.
+    }
+
+    public T? GetById(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().FirstOrDefault(predicate);
+    }
+
+    public T Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public T Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public T Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+}
