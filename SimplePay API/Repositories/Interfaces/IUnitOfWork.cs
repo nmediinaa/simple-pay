43d18ac8c46d85e6d8bab2152019aec39a2081a0
nmediinaa <nicolascostam23@gmail.com>
2026-07-10 12:23:19 -0300

    :sparkles: feat: Add unit of work pattern to centralize transactions

---
+++ SimplePay API/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using SimplePay_API.Models;
+
+namespace SimplePay_API.Repositories.Interfaces;
+
+public interface IUnitOfWork
+{
+    Repository<Account> AccountRepository { get; }
+
+    Repository<User> UserRepository { get; }
+
+    void Commit();
+
+}
---
+++ SimplePay API/Repositories/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using SimplePay_API.Context;
+using SimplePay_API.Models;
+using SimplePay_API.Repositories.Interfaces;
+
+namespace SimplePay_API.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+
+    private Repository<Account> _accountRepo;
+    private Repository<User> _userRepo;
+
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Repository<Account> AccountRepository
+    {
+        get
+        {
+            return _accountRepo ?? new Repository<Account>(_context);
+        }
+    }
+
+
+    public Repository<User> UserRepository
+    {
+        get
+        {
+            return _userRepo ?? new Repository<User>(_context);
+        }
+    }
+
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
